4fa913dc957e1d3be1a720f8cc3a0406d16a7212
Michal Dziurowski <mdziurowski@gmail.com>
2017-11-26 14:45:28 +0100

    responding with cached app

--- service-worker.js
+++ service-worker.js
@@ -15,3 +15,17 @@ const urlsToCache = [
         .then(cache => cache.addAll(urlsToCache))
     );
   });
+
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('dailyRusk') && key !== APPSHELL_CACHE_NAME).map(key => caches.delete(key))))
+    );
+  });
+
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+        })
+      );   
+  });
